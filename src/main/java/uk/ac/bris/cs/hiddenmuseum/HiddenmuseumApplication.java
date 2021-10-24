d3451330e113748674d21cdd78593ce25120b10d
baconsenpaii <baconforevery2@gmail.com>
2021-10-24 23:47:47 +0100

    added skeleton spring boot project

---
+++ src/main/java/uk/ac/bris/cs/hiddenmuseum/HiddenmuseumApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.bris.cs.hiddenmuseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddenmuseumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiddenmuseumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/bris/cs/hiddenmuseum/HiddenmuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.bris.cs.hiddenmuseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenmuseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
