3c58392d1fba6c6ee2f4359760e5e85064e0eae1
James-Millan <nf20792@bristol.ac.uk>
2021-11-22 11:05:16 +0000

    project builds successfully

--- src/main/java/uk/ac/bris/cs/hiddenmuseum/HiddenmuseumApplication.java
+++ src/main/java/uk/ac/bris/cs/hiddenmuseum/HiddenmuseumApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HiddenmuseumApplication {
-
+/*
 	public static void main(String[] args) {
 		SpringApplication.run(HiddenmuseumApplication.class, args);
 	}
+
+ */
 
 }
