32e0838382fdfe5987e5fe0ab46386f4fef8299a
James-Millan <nf20792@bristol.ac.uk>
2021-11-26 11:44:55 +0000

    integrating the demo and main project

---
+++ src/main/java/com/hiddenmuseumdemo/controller/SearchController.java
@@ -0,0 +1,17 @@
+package com.hiddenmuseumdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SearchController {
+
+    @GetMapping("/search")
+    public String searchTest(@RequestParam(defaultValue="test", required=false) String query, Model  model)  {
+        model.addAttribute("query", query);
+        return "search";
+    }
+	
+}
