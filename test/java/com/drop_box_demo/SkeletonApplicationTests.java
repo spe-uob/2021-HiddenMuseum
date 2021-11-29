8c44107e84a10bc106f919936250918cbd22ce6d
zxj0904 <78478159+zxj0904@users.noreply.github.com>
2021-11-29 23:53:44 +0800

    Add files via upload

---
+++ test/java/com/drop_box_demo/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drop_box_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
