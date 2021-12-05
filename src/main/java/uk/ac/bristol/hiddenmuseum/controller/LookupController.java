a8a3d512d3c42e402a4c8005e27b8e15503740f4
baconsenpaii <baconforevery2@gmail.com>
2021-12-05 23:46:09 +0000

    add lookup controller javadoc

--- src/main/java/uk/ac/bristol/hiddenmuseum/controller/LookupController.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/controller/LookupController.java
@@ -1,21 +1,25 @@
 package uk.ac.bristol.hiddenmuseum.controller;
 
-import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import uk.ac.bristol.hiddenmuseum.requests.LookupRequestBuilder;
-import uk.ac.bristol.hiddenmuseum.requests.RequestBuilder;
-import uk.ac.bristol.hiddenmuseum.requests.SearchRequestBuilder;
-import uk.ac.bristol.hiddenmuseum.service.FieldImpl;
 
-import java.util.LinkedHashMap;
-
+/**
+ * Controller for looking up individual items in the database
+ */
 @Controller
 public class LookupController {
 
+    /**
+     * Request handler for item lookups
+     *
+     * @param dataset id of the dataset being searched
+     * @param recordID id of the specific record being looked up
+     * @param model spring model
+     * @return view to use
+     */
     @GetMapping("/item/{dataset}/{id}")
     public String itemLookup(
             @PathVariable("dataset") String dataset,
