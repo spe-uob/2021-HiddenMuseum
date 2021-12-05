5be0cc1960a56a0c3aaa3cf46bf09e5ec08bcdfb
baconsenpaii <baconforevery2@gmail.com>
2021-12-05 20:28:11 +0000

    add javadoc comments

--- src/main/java/uk/ac/bristol/hiddenmuseum/requests/RequestBuilder.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/requests/RequestBuilder.java
@@ -12,9 +12,25 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Request building interface
+ *
+ * @param <T> type of the response
+ */
 public interface RequestBuilder<T> {
 
+    /**
+     * Returns the URL that will be queried by the request
+     *
+     * @return URL
+     */
     public String getUrl();
+
+    /**
+     * Send the completed request to the API, returns the response
+     *
+     * @return appropriate Java object for this request
+     */
     public T sendRequest();
 
 }
--- src/main/java/uk/ac/bristol/hiddenmuseum/requests/SearchResponse.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/requests/SearchResponse.java
@@ -3,6 +3,9 @@ package uk.ac.bristol.hiddenmuseum.requests;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Object returned by a search request
+ */
 public class SearchResponse implements Serializable {
 
     public int nhits;
