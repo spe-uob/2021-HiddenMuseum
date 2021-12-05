0b304f505c3d92f7d1fea1330989fc05c16df1d7
baconsenpaii <baconforevery2@gmail.com>
2021-12-05 20:31:11 +0000

    change some debug code

--- src/main/java/uk/ac/bristol/hiddenmuseum/controller/RequestTController.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/controller/RequestTController.java
@@ -2,6 +2,7 @@ package uk.ac.bristol.hiddenmuseum.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.ac.bristol.hiddenmuseum.requests.LookupRequestBuilder;
 import uk.ac.bristol.hiddenmuseum.requests.RequestBuilder;
 import uk.ac.bristol.hiddenmuseum.requests.SearchRequestBuilder;
 
@@ -10,9 +11,11 @@ public class RequestTController {
 
     @GetMapping("/hi")
     public String resquestT() {
-        var srq = new SearchRequestBuilder("https://opendata.bristol.gov.uk/",
-                "open-data-gallery-3-european-old-masters");
-        return srq.sendRequest();
+        var lrq = new LookupRequestBuilder("https://opendata.bristol.gov.uk/",
+                "open-data-gallery-3-european-old-masters",
+                "f67b626ecf21f59c6b319f90ea62cce75d27572c");
+        var response = lrq.sendRequest();
+        return response.datasetid + " " + response.recordid + " " + response.fields.toJSONString();
     }
 
 }
