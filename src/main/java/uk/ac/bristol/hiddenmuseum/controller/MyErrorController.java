ac6d2de62b51db3ba78f362586188e543b8157e1
baconsenpaii <baconforevery2@gmail.com>
2021-12-05 17:46:11 +0000

    MyErrorController javadoc + cosmetics

--- src/main/java/uk/ac/bristol/hiddenmuseum/controller/MyErrorController.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/controller/MyErrorController.java
@@ -8,25 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Controller to handle invalid HTTP requests
+ */
 @Controller
-
 public class MyErrorController implements ErrorController {
 
+    /**
+     * Request handler for invalid HTTP requests
+     * Returns the correct error page depending on what the error is
+     *
+     * @param request faulty HTTP request
+     * @return appropriate error page
+     */
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
 
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 return "error-500";
             }
         }
+
         return "error-404";
     }
 }
