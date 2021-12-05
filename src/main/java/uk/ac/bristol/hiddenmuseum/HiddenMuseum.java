5807b8ba6e93eb566c401ad53e4c1afeb2948da3
baconsenpaii <baconforevery2@gmail.com>
2021-12-05 16:28:01 +0000

    rename project to hidden museum

--- src/main/java/uk/ac/bristol/hiddenmuseum/HiddenMuseum.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/HiddenMuseum.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkeletonApplication {
+public class HiddenMuseum {
 
     public static void main(String[] args) {
-        SpringApplication.run(SkeletonApplication.class, args);
+        SpringApplication.run(HiddenMuseum.class, args);
     }
 
 }
