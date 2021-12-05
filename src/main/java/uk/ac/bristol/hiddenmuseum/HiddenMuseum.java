de661fba0405a0a3e7bc04b98c9be729a344f85d
baconsenpaii <baconforevery2@gmail.com>
2021-12-05 17:27:37 +0000

    add javadoc to HiddenMuseum.java

--- src/main/java/uk/ac/bristol/hiddenmuseum/HiddenMuseum.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/HiddenMuseum.java
@@ -3,9 +3,19 @@ package uk.ac.bristol.hiddenmuseum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hidden Museum web app
+ *
+ * @see <a href="https://github.com/spe-uob/2021-HiddenMuseum">spe-uob/2021-HiddenMuseum</a>
+ */
 @SpringBootApplication
 public class HiddenMuseum {
 
+    /**
+     * Entry point for the application
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(HiddenMuseum.class, args);
     }
