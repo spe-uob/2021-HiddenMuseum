bacb340c43912157d6e7ca63ba26815518f7d999
James-Millan <nf20792@bristol.ac.uk>
2022-02-11 17:27:42 +0000

    export works when you go to /export export button needs to be made and implemented for singular items

--- src/main/java/uk/ac/bristol/hiddenmuseum/controller/IndexController.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/controller/IndexController.java
@@ -1,9 +1,9 @@
 package uk.ac.bristol.hiddenmuseum.controller;
 
-import uk.ac.bristol.hiddenmuseum.requests.SchemaRequestBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import uk.ac.bristol.hiddenmuseum.requests.SchemaRequestBuilder;
 
 /**
  * Controller for the index page of the program
@@ -19,7 +19,6 @@ public class IndexController {
      * @return
      */
     @GetMapping("/")
-
     public String index(String name, Model model)  {
         var srq = new SchemaRequestBuilder("https://opendata.bristol.gov.uk/", "open-data-gallery-3-european-old-masters");
         var response = srq.sendRequest();
@@ -28,13 +27,6 @@ public class IndexController {
         model.addAttribute("fieldList", fieldList);
         model.addAttribute("name", name);
         return "index";
-    }
-    public void setFields(JSONObject field) {
-        this.fields = field;
-    }
-
-    public JSONObject getFields()   {
-        return this.fields;
     }
 
 }
--- src/main/java/uk/ac/bristol/hiddenmuseum/service/CSVBuilder.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/service/CSVBuilder.java
@@ -1,11 +1,10 @@
 package uk.ac.bristol.hiddenmuseum.service;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.ac.bristol.hiddenmuseum.controller.IndexController;
+import uk.ac.bristol.hiddenmuseum.requests.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 
@@ -13,33 +12,44 @@ public class CSVBuilder {
     @Autowired
     private IndexController indexController = new IndexController();
 
-    public JSONObject getJson() {
-        return this.indexController.getFields();
+    public List<String> getFields() {
+        SchemaRequestBuilder srb = new SchemaRequestBuilder("https://opendata.bristol.gov.uk/",
+                "open-data-gallery-3-european-old-masters");
+        SchemaResponse response = srb.sendRequest();
+        List<String> fields = new ArrayList<>();
+
+        for (SchemaField field : response.fields) {
+            fields.add(field.name);
+        }
+        return fields;
+        //response.fields.stream().map(f -> f.name).toArray();
+    }
+
+    public List<SearchRecord> getRecords() {
+        List<SearchRecord> records = new ArrayList<>();
+        SearchRequestBuilder srq = new SearchRequestBuilder("https://opendata.bristol.gov.uk/",
+                "open-data-gallery-3-european-old-masters");
+        srq.setLimit(1000);
+        SearchResponse response = srq.sendRequest();
+        return response.records;
     }
 
     //tab is a delimiter for this CSV
     public String getCSV()  {
-
-        JSONObject json = getJson();
-        System.out.println(json);
-        Iterator<String> keys = (Iterator<String>) json.keySet().iterator();
-        List<String> fieldList = new ArrayList<>();
-
-        while(keys.hasNext()){
-            String key = keys.next();
-            fieldList.add(key);
-        }
+        List<String> fieldList = getFields();
         StringBuilder csv = new StringBuilder();
         for(String field : fieldList)   {
             csv.append(field);
             csv.append("\t");
         }
-        for (JSONObject jsonChild : (Iterable<JSONObject>) json.values()) {
+        List<SearchRecord> data = getRecords();
+        for (SearchRecord record : data) {
             for (String field : fieldList) {
-                csv.append(jsonChild.getOrDefault(field, "DATA UNAVAILABLE"));
+                csv.append(record.fields.getOrDefault(field, "DATA UNAVAILABLE"));
                 csv.append("\t");
             }
         }
+
         return csv.toString();
 
 
