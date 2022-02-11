778cff7aef4a33f539f9068f9c69c1027d84355d
Dan <118icefire@gmail.com>
2022-02-11 17:23:00 +0000

    make html graph display data recieved from infographicController

--- src/main/java/uk/ac/bristol/hiddenmuseum/controller/InfographicController.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/controller/InfographicController.java
@@ -16,20 +16,22 @@ public class InfographicController {
     @GetMapping("/infographics")
     public String Info(Model model){
         var srq = new SearchRequestBuilder("https://opendata.bristol.gov.uk/", "open-data-gallery-3-european-old-masters");
-        srq.setQuery("");
         var response = srq.sendRequest();
         var listRecords = response.records;
         List<String> dates = new ArrayList<String>();
         List<Integer> intDates = new ArrayList<Integer>();
+        
         /*convert the dates to integers*/
         for (int i=0;i<listRecords.size();i++ ){
             dates.add(listRecords.get(i).fields.get("year_of_creation").toString());
         }
         /*find the highest and lowest dates*/
+        
         Integer high = 0;
         Integer low = 9999;
         for (String date : dates){
             if (date.length()==4){ /*only add if its just the 4 numbers, other formats exist like "about 1970" but those are ignored*/
+            
                 intDates.add(Integer.parseInt(date));
                 if (Integer.parseInt(date) > high){
                     high = Integer.parseInt(date);
@@ -40,6 +42,7 @@ public class InfographicController {
             }
         }
         /*make a list of all the numbers from highest to lowest and instantiate the frequency of each date as 0*/
+        
         List<Integer> datesToInclude = new ArrayList<Integer>();
         List<Integer> numOfDates = new ArrayList<Integer>();
         for (int i=low;i<=high;i++){
@@ -47,13 +50,14 @@ public class InfographicController {
             numOfDates.add(0);
         }
         /*calculating the frequency of each date*/
+        
         for(Integer date : intDates){
             numOfDates.set(date-low, numOfDates.get(date-low)+1);
         }
 
-
         model.addAttribute("datesToInclude", datesToInclude);
-        model.addAttribute(" numOfDates",  numOfDates);
+        model.addAttribute("numOfDates",  numOfDates);
+        System.out.println(datesToInclude);
+        System.out.println(numOfDates);
         return "infographics";
-    }
-}
+    }}
