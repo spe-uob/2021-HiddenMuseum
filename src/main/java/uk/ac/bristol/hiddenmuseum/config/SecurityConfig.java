1047dfe17f15cec270251d6237bf7a8216874643
James-Millan <nf20792@bristol.ac.uk>
2022-02-19 21:17:28 +0000

    javadocs for the security config

--- src/main/java/uk/ac/bristol/hiddenmuseum/config/SecurityConfig.java
+++ src/main/java/uk/ac/bristol/hiddenmuseum/config/SecurityConfig.java
@@ -5,9 +5,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Configuration to handle http connections
+ */
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * @param http incoming http connection
+     *
+     * Authorizes any http requests by default.
+     *
+     */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
